2c570207af656ad92691164a3c70f3f5592bd9ff
katalsupriya <katalsupriya36@gmail.com>
2019-04-02 19:28:26 +0530

    Done with resolving Conflicts Controller

--- BeerTickets.API/Controllers/BeertixapiController.cs
+++ BeerTickets.API/Controllers/BeertixapiController.cs
@@ -9,6 +9,41 @@ using System.Web.Http;
 
 namespace BeerTicket.API.Controllers
 {
+    //    [RoutePrefix("Beertixapi/v1.2")]
+    //    public class BeerTixApiController : Controller
+    //    {
+    //        [HttpPost]
+    //        public object Authenticate(ApiUsersViewModel apiUsersViewModel)
+    //        {
+    //            SunwingVouchersEntities dbContext = new SunwingVouchersEntities();
+    //            var user = dbContext.ApiUsers.Where(x => x.UserName == apiUsersViewModel.UserName &&
+    //             x.Password == apiUsersViewModel.Password && !x.IsDeleted).FirstOrDefault();
+    //            if (user != null)
+    //            {
+    //                // make the tocken and add the value of expireis  from WebConfig 
+    //            }
+
+    //            return new object
+    //            {
+
+    //            };
+    //        }
+
+    //        [Route("issuance")]
+    //        [HttpPost]
+    //        public string Issuance(string action)
+    //        {
+    //            if (action == "authenticate")
+    //            {
+    //                return "Ok";
+    //            }
+    //            else
+    //            {
+    //                return "Invalid Action";
+    //            }
+    //        }
+    //    }
+    //}
     [RoutePrefix("api/v1.2/issuance")]
     public class IssuanceController : ApiController
     {
